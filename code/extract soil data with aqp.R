5850bc90d2948289ba8a6df74d2a9573b931b04a
Ranae Dietzel <ranae.dietzel@gmail.com>
2017-07-20 15:44:59 -0500

    Need to add list binding within function

--- code/extract soil data with aqp.R
+++ code/extract soil data with aqp.R
@@ -1,36 +1,19 @@
 library(aqp)
 library(soilDB)
-library(compositions)
-library(reshape2)
-library(latticeExtra)
 library(plyr)
 
-# get sand, silt, clay data from the named series
-# return weighted mean of top 10cm
-get.data <- function(series) {
-  x <- fetchKSSL(series)
-  s <- slab(x, pedon_key ~ clay + sand + silt + oc + ph_h2o, slab.structure=c(0, 10), strict = FALSE, slab.fun = mean, na.rm=TRUE)
-  s <- dcast(s, pedon_key ~ variable, value.var = 'value')
-  ssc <- na.omit(s[, c('sand', 'silt', 'clay', 'oc' 'ph')])
-  return(ssc)
+get_soil_data <- function(series){
+  horizons(fetchKSSL(series))%>%
+    select(pedon_key, hzn_desgn, sand, silt, clay, oc, ph_h2o)%>%
+    filter(grepl('A', hzn_desgn))%>%
+    select(-c(hzn_desgn, pedon_key))%>%
+    summarise_each(funs(mean(., na.rm = TRUE)))%>%
+    mutate(soil = series)
+  
 }
 
-# combine original texture + simulated textures from normal composition
-sim.data <- function(ssc, n.sim=1000) {
-  # convert to compositional class, note range is now [0,1]
-  ssc.acomp <- acomp(ssc)
-  # simulate normally-distributed composition based on data
-  # note that var() is dispatched to var.acomp()
-  ssc.sim <- rnorm.acomp(n=n.sim, mean=meanCol(ssc.acomp), var=var(ssc.acomp))
-  # convert to data.frame and original range of [0,100]
-  ssc.sim <- as.data.frame(unclass(ssc.sim) * 100)
-  # return stacked original + simulated data
-  return(make.groups(original=ssc, simulated=ssc.sim))
-}
+soil_list<-c("howard", "webster", "canisteo")
 
+soils<-ldply(lapply(soil_list, get_soil_data))
 
-# get data for named series
-s.list <- c('clarksville', 'vista', 'auburn', 'cecil', 'drummer', 'capay')
-d <- lapply(s.list, get.data)
-names(d) <- s.list
-d <- ldply(d)
+soils<-bind_rows(lapply(soil_list, get_soil_data))
