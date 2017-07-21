d7214f4367c7eca9e30a26d8cbf9bbceaeaaf0c6
Ranae Dietzel <ranae.dietzel@gmail.com>
2017-07-21 16:11:14 -0500

    ARS flux and SSURGO data together at last

--- code/extract soil data with aqp.R
+++ code/extract soil data with aqp.R
@@ -3,7 +3,7 @@ library(soilDB)
 library(plyr)
 
 get_soil_data <- function(series){
-  horizons(fetchKSSL(series))%>%
+  l<-horizons(fetchKSSL(series))%>%
     select(pedon_key, hzn_desgn, sand, silt, clay, oc, ph_h2o)%>%
     filter(grepl('A', hzn_desgn))%>%
     select(-c(hzn_desgn, pedon_key))%>%
@@ -13,7 +13,5 @@ get_soil_data <- function(series){
 }
 
 soil_list<-c("howard", "webster", "canisteo")
-
-soils<-ldply(lapply(soil_list, get_soil_data))
 
 soils<-bind_rows(lapply(soil_list, get_soil_data))
